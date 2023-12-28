2c36b5bfdcc00e06d3897d18d6b93efa1388cc42
Amarildo <amarildos@outloo.it>
2023-12-28 18:43:18 +0100

    my ladder added

---
+++ js/array_tail/script.js
@@ -0,0 +1,15 @@
+/* Creiamo un array chiedendo all'utente quanti elementi dovrà 
+contenere e generando tanti numeri casuali (compresi nell'intervallo da 1 a 100) 
+per quanti sono gli elementi da inserire.
+*/
+// Stampiamo poi gli ultimi 5 elementi dell'Array
+// Chiediamo all'utente quanti elementi dell'array dovremo stampare
+// controllare che il numero di elementi della coda non sia più grande del numero di elementi totali dell'array
+
+
+//1. chiediamo all'utente di inserire il numero di elementi che desidera nell'array.
+//2. Controlliamo se l'input dell'utente è un numero valido e se è maggiore di zero
+//3. Creazione dell'array con numeri casuali compresi tra 1 e 100
+//4. Stampiamo gli ultimi 5 elementi dell'array
+//5. Chiediamo all'utente quanti elementi vuole stampare
+//6. Verifica se l'input dell'utente è valido
